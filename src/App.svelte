b9945933cf0c3602db4f9159649a45157039b97c
Alastair Otter <alastair@mediahack.co.za>
2021-10-18 21:28:08 +0200

    Adding palette dropdown

--- src/App.svelte
+++ src/App.svelte
@@ -1,25 +1,22 @@
 <script>
   let start = 0
-  let end = 1
-  let palette = 'YlGnBu'
+  let end = 1000
+  let palette
   let newPalette = palette
   import chroma from 'chroma-js'
   let scale = chroma.scale(palette).domain([start, end])
-  let values = ''
+  let values = '100,200,300,400,500,600,700,800,900'
   let inputValues = []
-  // let count = [
-  //   867970, 337389, 1316180, 1355122, 763410, 535943, 458201, 145778, 667080,
-  // ]
-  $: console.log(palette)
   $: scale = chroma.scale(palette).domain([start, end])
-  $: console.log(inputValues)
+  import { colors } from '../data/colors.js'
+
+  import ColorPalettes from './ColorPalettes.svelte'
 
   $: inputValues = inputValues
   $: convertValues(values)
 
   function convertValues(values) {
     let v = values.replaceAll(' ', '')
-
     inputValues = v.split(',')
   }
 
@@ -31,73 +28,147 @@
     palette = newPalette
   }
 
+  let infoRange = false
+  let infoMinMax = false
+
   // $: console.log(values)
 </script>
 
 <main>
-  <div class="inputs">
-    Start: <input type="text" bind:value={start} /> &nbsp; End:
-    <input type="text" bind:value={end} /><button on:click={minMax}
-      >Set Min-Max
-    </button>
+  <h1>ColourScales</h1>
+  <div class="intro">
+    A utility tool for generating colours based on a range of numbers. Replace
+    the values in the "Number Range" with your selected numbers and add a
+    minimum or maximum value for your range.
   </div>
-  <div class="inputs">
-    Values: <input type="text" class="values" bind:value={values} />
-  </div>
-  <div class="inputs">
-    Palette: <input
-      type="text"
-      class="palette"
-      bind:value={newPalette}
-    /><button on:click={updatePalette}>Change Palette</button>
-  </div>
-  <div class="colors">
-    {#each inputValues as c}
-      <div class="box-wrap">
-        <div class="box" style="background: {scale(c)};" />
-        <div class="label">{c}</div>
-        <div class="label">{scale(c)}</div>
+  <div class="input-wrap">
+    <div class="input-row">
+      Start Value: <input type="text" bind:value={start} /> &nbsp; End Value:
+      <input type="text" bind:value={end} /><button on:click={minMax}
+        >Set Min-Max
+      </button>
+      <div class="info">
+        <!-- svelte-ignore a11y-mouse-events-have-key-events -->
+        <img
+          src="./assets/icons/info.svg"
+          alt="info"
+          on:mouseover={() => {
+            infoMinMax = !infoMinMax
+          }}
+          on:mouseout={() => {
+            infoMinMax = !infoMinMax
+          }}
+        />
       </div>
-    {/each}
+      {#if infoMinMax}
+        <div class="description">
+          <div>
+            You can either provide starting/ending values here, or you can
+            calculate them automatically based on the numbers added below.
+          </div>
+        </div>
+      {/if}
+    </div>
+    <div class="input-row">
+      Number Range: <input
+        type="text"
+        class="values values-long"
+        bind:value={values}
+      />
+      <div class="info">
+        <!-- svelte-ignore a11y-mouse-events-have-key-events -->
+        <img
+          src="./assets/icons/info.svg"
+          alt="info"
+          on:mouseover={() => {
+            infoRange = !infoRange
+          }}
+          on:mouseout={() => {
+            infoRange = !infoRange
+          }}
+        />
+      </div>
+      {#if infoRange}
+        <div class="description">
+          <div>Add a list of comma-separated numbers here.</div>
+        </div>
+      {/if}
+    </div>
+    <div class="input-row">
+      Choose Palette:
+      <select bind:value={palette}>
+        {#each colors as c}
+          <option>{c}</option>
+        {/each}
+      </select>
+    </div>
+    <div class="colors">
+      {#each inputValues as c}
+        <div class="box-wrap">
+          <div class="box" style="background: {scale(c)};" />
+          <div class="label">{c}</div>
+          <div class="label">{scale(c)}</div>
+        </div>
+      {/each}
+    </div>
   </div>
 
-  <img src="./assets/images/palettes.png" alt="palettes" />
-  <!-- {#each inputValues as v}
-    <div>{v}</div>
-  {/each} -->
+  <ColorPalettes />
 </main>
 
 <style>
   main {
-    width: 600px;
+    width: 700px;
     margin-left: auto;
     margin-right: auto;
     margin-top: 50px;
   }
-  .values {
-    width: 500px;
+  .input-row {
+    position: relative;
+    margin: 10px 0px;
+    padding: 20px 10px;
+    border: dotted 1px lightgray;
+    /* background: #eee; */
   }
-  .inputs {
+  .description {
+    position: absolute;
+    background: #000;
+    padding: 10px;
+    color: #fff;
+    max-width: 300px;
+    top: 0px;
+    right: 30px;
     font-size: 0.8rem;
-    margin-bottom: 10px;
+    margin-top: 5px;
+    margin-bottom: 0px;
+    z-index: 200;
   }
+  .info {
+    position: absolute;
+    top: 5px;
+    right: 5px;
+    text-align: right;
+    padding: 5px;
+  }
+  .info img {
+    width: 15px;
+    opacity: 0.3;
+    cursor: pointer;
+  }
+  .info img:hover {
+    opacity: 1;
+  }
+
   .colors {
     display: grid;
-    grid-template-columns: repeat(9, auto);
-    grid-gap: 10px;
-    float: left;
+    grid-template-columns: repeat(10, 1fr);
   }
   .box {
     width: 50px;
     height: 50px;
-    border: solid 1px lightgray;
-    font-size: 0.8rem;
   }
   .label {
-    font-size: 0.7rem;
+    font-size: 0.8rem;
     text-align: center;
-  }
-  button {
-    margin-left: 10px;
   }
 </style>
